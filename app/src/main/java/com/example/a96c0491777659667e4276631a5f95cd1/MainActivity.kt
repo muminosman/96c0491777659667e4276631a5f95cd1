6acf85d8e4b55d4c455a644002f9c49b95423c71
muminosman <muminoth@gmail.com>
2021-03-28 17:52:31 +0300

    created object

---
+++ app/src/main/java/com/example/a96c0491777659667e4276631a5f95cd1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a96c0491777659667e4276631a5f95cd1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
